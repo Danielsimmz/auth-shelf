ab2bec02752f983ec8205b8c84ec38069443e893
Daniel Simango <daniel.simmz@gmail.com>
2020-07-15 13:48:09 -0500

    setup component for feedback table

--- src/components/FeedbackForm/FeedbackForm.js
+++ src/components/FeedbackForm/FeedbackForm.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button } from "@material-ui/core";
+import { Button, Table, TableBody, TableCell } from "@material-ui/core";
 import { withRouter } from "react-router";
+import "./feedbackForm.css"
 
 class FeedbackForm extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Table >
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Understanding</th>
+              <th>Quality</th>
+              <th>Interest</th>
+              <th>Comments</th>
+              <th></th>
+            </tr>
+          </thead>
+          <TableBody >
+          <tr>
+            <TableCell>{this.props.feedbacks.username}</TableCell>
+            <TableCell>{this.props.feedbacks.understanding}</TableCell>
+            <TableCell>{this.props.feedbacks.quality}</TableCell>
+            <TableCell>{this.props.feedbacks.interest}</TableCell>
+            <TableCell>{this.props.feedbacks.comments}</TableCell>
+          </tr>
+          </TableBody>
+        </Table>
+      </div>
+    );
   }
 }
 
