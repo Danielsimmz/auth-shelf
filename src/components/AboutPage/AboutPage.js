3d0bf14d37089ee8137868a5ef0fda14b66db691
Daniel Simango <daniel.simmz@gmail.com>
2020-07-17 12:30:25 -0500

    updated about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -7,17 +7,22 @@ import './AboutPage.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 const AboutPage = () => (
-  <div className="home">
+  <div>
     <div>
-      <p>
-        Hi. Ever wondered what it would be like to be a tennis guru. I'm not
-        talking about playing in the Roland Garros necessarily, but being able
-        to participate in friendly tournaments with friends and crushing it.
-        <br />
-        Well, you are in the right place. This site is dedicated to teach you in
-        all things tennis related and in no time you should be well on your way
-        to becoming the player of your dreams.
-      </p>
+      <form id="about">
+        <p>
+          <i>
+            Hi. Ever wondered what it would be like to be a tennis guru. I'm not
+            talking about playing in the Roland Garros necessarily, but being
+            able to participate in friendly tournaments with friends and
+            crushing it.
+            <br />
+            Well, you are in the right place. This site is dedicated to teach
+            you in all things tennis related and in no time you should be well
+            on your way to becoming the player of your dreams.
+          </i>
+        </p>
+      </form>
     </div>
   </div>
 );
